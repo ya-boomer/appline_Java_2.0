80b591fb100f6e5cc86c08ab5627cd8a5bbba3ef
Mishchenko Aleksey <mishchenko.an.26@gmail.com>
2021-03-02 00:21:38 +0300

    1. Реализовать базовые операции калькулятора (+,-,/,*) для предыдущего задания
    2. Поиск максимального элемента в массиве. Для начала задать массив слов.Размерность массива произвольна, задается в консоли. После чего в консоли вводятся слова в количестве равном заданной длине массива. В полученном массиве необходимо найти самое длинное слово. Результат вывести на консоль
    
    3. Программа должна выполнять одно из заданий на выбор. Если в консоли ввести 1 - запуститься выполнение калькулятора, если 2 - поиск максимального слова в массиве.

--- src/The_Calc.java
+++ src/The_Calc.java
@@ -1,15 +1,33 @@
 import java.util.Scanner;
 
 /**
- * @param x - first number
- * @param y - second number
- * @param z - arithmetic operation
+ * @param a      - operation number
+ * @param i      - array element number
+ * @param j      - the number of the maximum element of the array
+ * @param max    - maximum length element of the array
+ * @param x      - first number
+ * @param y      - second number
+ * @param z      - arithmetic operation
+ * @param result - result of calculations
  */
 public class The_Calc {
     public static void main(String[] args) {
-        //enter 2 number and arithmetic operation
+        System.out.print("введите что Вы хотите сделать:\n 1 - посчитать в калькуляторе\n 2 - найти максимальный элемент массива ");
         Scanner scanner = new Scanner(System.in);
+        byte a = scanner.nextByte();
+        if (a == 1) {
+            Calculate();
+        } else if (a == 2) {
+            System.out.println("Введите количество элементов массива");
+            int lengthArray = scanner.nextInt();
+            maxArrayElement(lengthArray);
+        } else System.out.println("Введено некорректное значение");
+    }
+
+    public static Object Calculate() {
+        //enter 2 number and arithmetic operation
         System.out.print("введите число x: ");
+        Scanner scanner = new Scanner(System.in);
         double x = scanner.nextDouble();
         System.out.print("введите число y: ");
         double y = scanner.nextDouble();
@@ -20,26 +38,40 @@ public class The_Calc {
         switch (z) {
             case "+":
                 result = x + y;
-                System.out.printf("%.4f", result);
-                break;
+                return String.valueOf(System.out.printf("%.4f", result));
             case "-":
                 result = x - y;
-                System.out.printf("%.4f", result);
-                break;
+                return String.valueOf(System.out.printf("%.4f", result));
             case "*":
                 result = x * y;
-                System.out.printf("%.4f", result);
-                break;
+                return String.valueOf(System.out.printf("%.4f", result));
             case "/":
                 result = x / y;
-                System.out.printf("%.4f", result);
-                break;
+                return String.valueOf(System.out.printf("%.4f", result));
             //incorrect arithmetic operation
             default:
-                System.out.print("действие невозможно, введите одну из доступных операций: +, -, *, /");
-                break;
+
+                return String.valueOf(System.out.printf("действие невозможно, введите одну из доступных операций: +, -, *, /"));
         }
 
+    }
+
+    public static String maxArrayElement(int lengthArray) {
+        int max, j = 0;
+        String[] myArray = new String[lengthArray];
+        for (int i = 0; i < lengthArray; i++) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Введите элемент массива");
+            myArray[i] = scanner.nextLine();
+        }
+        for (int i = 0; i < lengthArray; i++) {
+            max = myArray[0].length();
+            if (myArray[i].length() > max)
+                max = myArray[i].length();
+            j = i;
+
+        }
+        return String.valueOf(System.out.printf("Максимальный элемент массива %s%n", myArray[j]));
     }
 }
 
